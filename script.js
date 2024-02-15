77643ee3d415af07e0ef86211e877f9f3ce0b08e
PrasanPoudel <prasanpoudel501@gmail.com>
2024-02-15 22:42:05 +0545

    finished

--- script.js
+++ script.js
@@ -15,3 +15,30 @@ menu.onclick=()=>{
 window.onscroll = () => {
   navLinks.classList.remove('menu_active');
 }
+const dynamicText = document.querySelector(".typewriter");
+const words = ["UI/UX Designer","Graphic Designer"];
+
+// Variables to track the position and deletion status of the word
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChar;
+
+    if (!isDeleting && charIndex < currentWord.length) {
+        charIndex++;
+        setTimeout(typeEffect, 200);
+    } else if (isDeleting && charIndex > 0) {
+        charIndex--;
+        setTimeout(typeEffect, 100);
+    } else {
+        isDeleting = !isDeleting;
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200);
+    }
+}
+
+typeEffect();
